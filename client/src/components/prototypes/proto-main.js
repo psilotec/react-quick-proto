3b5df77035909f4ad1bab0c020334074f1ee1e73
psilotec <scottadevito@gmail.com>
2017-01-17 20:07:15 -0500

    ProtoMain formatting

--- client/src/components/prototypes/proto-main.js
+++ client/src/components/prototypes/proto-main.js
@@ -5,8 +5,10 @@ import ComponentBtn from './component-btn';
 class ProtoMain extends Component {
     render() {
         return (
-            <div className="proto-main flex">
+            <div className="proto-main-flex">
                 <h2>Proto Main</h2>
+                <ComponentBtn />
+                <ComponentBtn />
                 <ComponentBtn />
             </div>
         );
