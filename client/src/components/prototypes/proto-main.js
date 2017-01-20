dbb120affc03377ada0065e74e0e8084d98dfd8c
psilotec <scottadevito@gmail.com>
2017-01-20 00:05:29 -0500

    Create renderGridlines to handle dynamic className

--- client/src/components/prototypes/proto-main.js
+++ client/src/components/prototypes/proto-main.js
@@ -7,23 +7,29 @@ class ProtoMain extends Component {
     constructor(props) {
         super(props);
 
-        // State to keep track of grid lines checkbox
+        // State to keep track of grid lines checkbox and the classname for root element of this component
         this.state = {
             checked: false,
-            gridlines: ""
         };
 
-        this.handleChange = this.handleChange.bind(this);
+        // Binding so handleToggle method has access to setState
+        this.handleToggle = this.handleToggle.bind(this);
     }
 
-    handleChange(event) {
+    handleToggle() {
         this.setState({checked: !this.state.checked});
     }
-    render() {
-        console.log(this.state.checked);
+
+    renderGridlines() {
+        if (this.state.checked) {
+            return "proto-main-flex gridlines";
+        }
+        return "proto-main-flex";
+    }
+    render() {        
         return (
-            <div className="proto-main-flex">
-                <ToggleGridlines checked={this.state.checked} handleChange={this.handleChange} />
+            <div className={this.renderGridlines()}>
+                <ToggleGridlines handleToggle={this.handleToggle}/>
 
                 <h2>Project Name</h2>
                 
--- client/src/components/prototypes/toggle-gridlines.js
+++ client/src/components/prototypes/toggle-gridlines.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ToggleGridlines = ({checked, handleChange}) => {
+const ToggleGridlines = ({checked, handleToggle}) => {
 
     return (
         <div className="toggle-gridlines">
-            <div className="ui toggle checkbox" onClick={handleChange} >
-                <input type="checkbox" name="public" value={checked} />
+            <div className="ui toggle checkbox">
+                <input type="checkbox" name="public" value={checked} onClick={handleToggle} />
                 <label>Show grid lines</label>
             </div>
         </div>
