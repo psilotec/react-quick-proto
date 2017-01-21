418b6719fb77e40f3fa7a9b761ca62fa0e9b4a35
psilotec <scottadevito@gmail.com>
2017-01-21 13:21:12 -0500

    Layout formatting and styling

--- client/src/components/prototypes/proto-board.js
+++ client/src/components/prototypes/proto-board.js
@@ -5,10 +5,12 @@ import ComponentBtn from './component-btn';
 const ProtoBoard = ({renderGridlines}) => {
 
     return (
+        <div className="ui fluid container segment">
         <div className={renderGridlines()}>
                 <ComponentBtn />
                 <ComponentBtn />
                 <ComponentBtn />
+        </div>
         </div>
     );
 };
